2300c5f90a86735ddac49482de1aedd522a8fc1d
Ayham <ayhambusiness133@gmail.com>
2026-06-17 10:54:10 +0300

    adding api responses

---
+++ Responses/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUDPractice.Responses
+{
+    public class ApiResponse
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public List<string> Errors { get; set; } = new();
+    }
+
+}
---
+++ Responses/ApiResponseOfT.cs
@@ -0,0 +1,8 @@
+﻿namespace CRUDPractice.Responses
+{
+    public class ApiResponse<T> : ApiResponse
+    {
+        public T? Data { get; set; }
+    }
+
+}
